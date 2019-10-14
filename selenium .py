cfbe86b62fab9c5b551f69d9316526bc4cb8e1ab
xray01 <56517928+xray01@users.noreply.github.com>
2019-10-14 11:19:23 +0330

    delete

--- selenium .py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-import time
-browser = webdriver.Firefox(executable_path = r'C:\Users\DANIAL\Desktop\New folder\geckodriver.exe')
-browser.get('https://www.google.com') 
-time.sleep(10)
-browser.quit()
